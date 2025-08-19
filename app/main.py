8f81ff9fcf688c29d2acd73c4880e81b32586dae
Alex_Lushenko <mralealex9@gmail.com>
2025-08-19 19:51:35 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,10 @@
 def copy_file(command: str) -> None:
-    lines = command.split()
-    if lines[0] != "cp" or len(lines) != 3:
+    line = command.split()
+    if not line or line[0] != "cp" or len(line) != 3:
         return
-
-    source_file, target_file = lines[1], lines[2]
+    source_file, target_file = line[1], line[2]
     if source_file == target_file:
         return
-
     try:
         with open(source_file, "r") as file_in, \
                 open(target_file, "w") as file_out:
